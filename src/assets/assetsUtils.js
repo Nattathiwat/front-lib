2b9c66475860169995ac94cb1b6b7022dc89cbf3
Joe <parinya27611@gamil.com>
2023-10-27 12:48:56 +0700

    edit assetsUtils

--- src/assets/assetsUtils.js
+++ src/assets/assetsUtils.js
@@ -1,11 +1,19 @@
 
 const months = [ 'ม.ค.', 'ก.พ.', 'มี.ค.', 'เม.ย.', 'พ.ค.', 'มิ.ย.', 'ก.ค.', 'ส.ค.', 'ก.ย.', 'ต.ค.', 'พ.ย.', 'ธ.ค.',]; 
 const months_Long = [ "มกราคม", "กุมภาพันธ์", "มีนาคม", "เมษายน", "พฤษภาคม", "มิถุนายน", "กรกฎาคม", "สิงหาคม", "กันยายน", "ตุลาคม", "พฤศจิกายน", "ธันวาคม", ];
-
+const types = {
+  xlsx: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',csv: 'text/csv;charset=utf-8', docx: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', abs: 'audio/x-mpeg', ai: 'application/postscript', aif: 'audio/x-aiff', aifc: 'audio/x-aiff', aiff: 'audio/x-aiff', aim: 'application/x-aim', art: 'image/x-jg', asf: 'video/x-ms-asf', asx: 'video/x-ms-asf', au: 'audio/basic', avi: 'video/x-msvideo', avx: 'video/x-rad-screenplay', bcpio: 'application/x-bcpio', bin: 'application/octet-stream', bmp: 'image/bmp', body: 'text/html', cdf: 'application/x-cdf', cer: 'application/pkix-cert', class: 'application/java', cpio: 'application/x-cpio', csh: 'application/x-csh', css: 'text/css', dib: 'image/bmp', doc: 'application/msword', dtd: 'application/xml-dtd', dv: 'video/x-dv', dvi: 'application/x-dvi', eot: 'application/vnd.ms-fontobject', eps: 'application/postscript', etx: 'text/x-setext', exe: 'application/octet-stream', gif: 'image/gif', gtar: 'application/x-gtar', gz: 'application/x-gzip', hdf: 'application/x-hdf', hqx: 'application/mac-binhex40', htc: 'text/x-component', htm: 'text/html', html: 'text/html', ief: 'image/ief', jad: 'text/vnd.sun.j2me.app-descriptor', jar: 'application/java-archive', java: 'text/x-java-source', jnlp: 'application/x-java-jnlp-file', jpe: 'image/jpeg', jpeg: 'image/jpeg', jpg: 'image/jpeg', js: 'application/javascript', jsf: 'text/plain', json: 'application/json', jspf: 'text/plain', kar: 'audio/midi', latex: 'application/x-latex', m3u: 'audio/x-mpegurl', mac: 'image/x-macpaint', man: 'text/troff', mathml: 'application/mathml+xml', me: 'text/troff', mid: 'audio/midi', midi: 'audio/midi', mif: 'application/x-mif', mov: 'video/quicktime', movie: 'video/x-sgi-movie', mp1: 'audio/mpeg', mp2: 'audio/mpeg', mp3: 'audio/mpeg', mp4: 'video/mp4', mpa: 'audio/mpeg', mpe: 'video/mpeg', mpeg: 'video/mpeg', mpega: 'audio/x-mpeg', mpg: 'video/mpeg', mpv2: 'video/mpeg2', ms: 'application/x-wais-source', nc: 'application/x-netcdf', oda: 'application/oda', odb: 'application/vnd.oasis.opendocument.database', odc: 'application/vnd.oasis.opendocument.chart', odf: 'application/vnd.oasis.opendocument.formula', odg: 'application/vnd.oasis.opendocument.graphics', odi: 'application/vnd.oasis.opendocument.image', odm: 'application/vnd.oasis.opendocument.text-master', odp: 'application/vnd.oasis.opendocument.presentation', ods: 'application/vnd.oasis.opendocument.spreadsheet', odt: 'application/vnd.oasis.opendocument.text', otg: 'application/vnd.oasis.opendocument.graphics-template', oth: 'application/vnd.oasis.opendocument.text-web', otp: 'application/vnd.oasis.opendocument.presentation-template', ots: 'application/vnd.oasis.opendocument.spreadsheet-template', ott: 'application/vnd.oasis.opendocument.text-template', ogx: 'application/ogg', ogv: 'video/ogg', oga: 'audio/ogg', ogg: 'audio/ogg', otf: 'application/x-font-opentype', spx: 'audio/ogg', flac: 'audio/flac', anx: 'application/annodex', axa: 'audio/annodex', axv: 'video/annodex', xspf: 'application/xspf+xml', pbm: 'image/x-portable-bitmap', pct: 'image/pict', pdf: 'application/pdf', pgm: 'image/x-portable-graymap', pic: 'image/pict', pict: 'image/pict', pls: 'audio/x-scpls', png: 'image/png', pnm: 'image/x-portable-anymap', pnt: 'image/x-macpaint', ppm: 'image/x-portable-pixmap', ppt: 'application/vnd.ms-powerpoint', pps: 'application/vnd.ms-powerpoint', ps: 'application/postscript', psd: 'image/vnd.adobe.photoshop', qt: 'video/quicktime', qti: 'image/x-quicktime', qtif: 'image/x-quicktime', ras: 'image/x-cmu-raster', rdf: 'application/rdf+xml', rgb: 'image/x-rgb', rm: 'application/vnd.rn-realmedia', roff: 'text/troff', rtf: 'application/rtf', rtx: 'text/richtext', sfnt: 'application/font-sfnt', sh: 'application/x-sh', shar: 'application/x-shar', sit: 'application/x-stuffit', snd: 'audio/basic', src: 'application/x-wais-source', sv4cpio: 'application/x-sv4cpio', sv4crc: 'application/x-sv4crc', svg: 'image/svg+xml', svgz: 'image/svg+xml', swf: 'application/x-shockwave-flash', t: 'text/troff', tar: 'application/x-tar', tcl: 'application/x-tcl', tex: 'application/x-tex', texi: 'application/x-texinfo', texinfo: 'application/x-texinfo', tif: 'image/tiff', tiff: 'image/tiff', tr: 'text/troff', tsv: 'text/tab-separated-values', ttf: 'application/x-font-ttf', txt: 'text/plain', ulw: 'audio/basic', ustar: 'application/x-ustar', vxml: 'application/voicexml+xml', xbm: 'image/x-xbitmap', xht: 'application/xhtml+xml', xhtml: 'application/xhtml+xml', xls: 'application/vnd.ms-excel', xml: 'application/xml', xpm: 'image/x-xpixmap', xsl: 'application/xml', xslt: 'application/xslt+xml', xul: 'application/vnd.mozilla.xul+xml', xwd: 'image/x-xwindowdump', vsd: 'application/vnd.visio', wav: 'audio/x-wav', wbmp: 'image/vnd.wap.wbmp', wml: 'text/vnd.wap.wml', wmlc: 'application/vnd.wap.wmlc', wmls: 'text/vnd.wap.wmlsc', wmlscriptc: 'application/vnd.wap.wmlscriptc', wmv: 'video/x-ms-wmv', woff: 'application/font-woff', woff2: 'application/font-woff2', wrl: 'model/vrml', wspolicy: 'application/wspolicy+xml', z: 'application/x-compress', zip: 'application/zip'
+};
 import breadcrumb from './breadcrumbs.js'
 
 export default {
+  getTypeFile(filename) {
+    //aaa.xlsx -> application/vnd.openxmlformats-officedocument.spreadsheetml.sheet
+    let type = filename.split('.').pop();
+    return types[type] ? types[type] : 'application/octet-stream'
+  },
   currentDate() {
+    //01/11/2566
     let d = new Date();
     let month = d.getMonth()+1;
     let day = d.getDate();
@@ -13,29 +21,43 @@ export default {
     return output
   },
   currentTime() {
+    //10:11
     let d = new Date();
     let h = d.getHours();
     let m = d.getMinutes();
     let output = (h<10 ? '0' : '') + h + ':' + (m<10 ? '0' : '') + m;
     return output
   },
+  numberWithCommas(data) {
+    //1111.11 -> 1,111.11
+    return data.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  },
+  yearDel543(data) {
+    //11/01/2566 -> 11/01/2023
+    return data ? data.split('/')[0]+'/'+data.split('/')[1]+'/'+(data.split('/')[2]-543) : ''
+  },
+  yearPlus543(data) {
+    //11/01/2023 -> 11/01/2566
+    return data ? data.split('/')[0]+'/'+data.split('/')[1]+'/'+(+(data.split('/')[2])+543) : ''
+  },
+  convertShortMonth(data) {
+    //11/11/2566 -> 11 พ.ย. 2566
+    if (data.split('/').length > 2) {
+      return data.split('/')[0] + ' ' + months[parseInt(data.split('/')[1])-1] + ' ' + data.split('/')[2]
+    } else {
+      return ''
+    }
+  },
   convertLongMonth(data) {
+    //11/11/2566 -> 11 พฤศจิกายน 2566
     if (data.split('/').length > 2) {
       return data.split('/')[0] + ' ' + months_Long[parseInt(data.split('/')[1])-1] + ' ' + data.split('/')[2]
     } else {
       return ''
     }
   },
-  commaNumber(data) {
-    return data.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-  },
-  year543(data) {
-    return data ? data.split('/')[0]+'/'+data.split('/')[1]+'/'+(data.split('/')[2]-543) : ''
-  },
-  yearPlus543(data) {
-    return data ? data.split('/')[0]+'/'+data.split('/')[1]+'/'+(+(data.split('/')[2])+543) : ''
-  },
-  covertDateByMonthThBbRev(monthThBbRev) {
+  covertDateFromMonthThaiShort(monthThBbRev) {
+    //monthThBbRev = 11 ม.ค. 2566 -> 11/01/2566
     let result = '';
     if (monthThBbRev) {
       if (monthThBbRev.split(' ').length > 3) {
@@ -52,7 +74,8 @@ export default {
     }
     return result;
   },
-  covertDateByMonthTh(monthThBbRev) {
+  covertDateFromMonthThaiLong(monthThBbRev) {
+    //monthThBbRev = 11 มกราคม 2566 -> 11/01/2566
     let result = '';
     if (monthThBbRev) {
       if (monthThBbRev.split(' ').length > 3) {
@@ -70,12 +93,15 @@ export default {
     return result;
   },
   longDateToShort(date) {
+    //fulldate -> 11/06/2566
     const day = date.getDate();
     const month = date.getMonth() + 1;
     const year = date.getFullYear() + 543;
+    
     return `${day > 9 ? day : '0' + day}/${month > 9 ? month : '0' + month}/${year}`;
   },
   formatNewDate(data) {
+    //11/06/2566 -> fulldate
     if(data) {
       if (data.split(' ').length > 1) {
         let newDate = data.split(' ')[0]
